df230d38ae5f0002a0c48e14bd9f3ddb56056036
Matthew Parker <mattheweparker@gmail.com>
2019-06-04 13:51:24 -0500

    lab finished

--- school.py
+++ school.py
@@ -1,1 +1,19 @@
 class School:
+    
+    def __init__(self, name, roster={}):
+        self.name = name
+        self.roster = roster
+    
+    def add_student(self, fullname, grade):
+        if grade in self.roster:
+            self.roster[grade].append(fullname)
+        else:
+            self.roster[grade] = [fullname]
+            
+    def grade(self, grade):
+        return self.roster[grade]
+    
+    def sort_roster(self):
+        for grade in self.roster:
+            self.roster[grade] = sorted(self.roster[grade])
+        return self.roster
